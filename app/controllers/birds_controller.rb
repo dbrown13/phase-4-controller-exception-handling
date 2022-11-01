61411cf50cc4616a482f5e52089b5b7751126086
Deryn Brown <Deryn.2017@gmail.com>
2022-11-01 16:57:49 -0500

    commit tree 93aabe4e56ddf37b0bc2a74fb3fceaedaa2c41c5

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,5 @@
 class BirdsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /birds
   def index
@@ -14,44 +15,44 @@ class BirdsController < ApplicationController
 
   # GET /birds/:id
   def show
-    bird = Bird.find_by(id: params[:id])
+    bird = find_bird
     if bird
       render json: bird
     else
-      render json: { error: "Bird not found" }, status: :not_found
+      render_not_found_response
     end
   end
 
   # PATCH /birds/:id
   def update
-    bird = Bird.find_by(id: params[:id])
+    bird = find_bird
     if bird
       bird.update(bird_params)
       render json: bird
     else
-      render json: { error: "Bird not found" }, status: :not_found
+      render_not_found_response
     end
   end
 
   # PATCH /birds/:id/like
   def increment_likes
-    bird = Bird.find_by(id: params[:id])
+    bird = find_bird
     if bird
       bird.update(likes: bird.likes + 1)
       render json: bird
     else
-      render json: { error: "Bird not found" }, status: :not_found
+      render_not_found_response
     end
   end
 
   # DELETE /birds/:id
   def destroy
-    bird = Bird.find_by(id: params[:id])
+    bird = find_bird
     if bird
       bird.destroy
       head :no_content
     else
-      render json: { error: "Bird not found" }, status: :not_found
+      render_not_found_response
     end
   end
 
@@ -59,6 +60,16 @@ class BirdsController < ApplicationController
 
   def bird_params
     params.permit(:name, :species, :likes)
+  end
+
+  private
+  def render_not_found_response
+    render json: { error: "Bird not found" }, status: :not_found
+  end
+
+  private
+  def find_bird
+    Bird.find(id: params[:id])
   end
 
 end
